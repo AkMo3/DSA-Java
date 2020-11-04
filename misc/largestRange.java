fee2fd42fc4d6781367331417b86af239e705435
Akash Mondal <69365878+AkMo3@users.noreply.github.com>
2020-11-04 17:20:49 +0530

    fix: format of longestRange

--- misc/largestRange.java
+++ misc/largestRange.java
@@ -4,13 +4,12 @@ public class largestRange {
   // Finds the length of longest occurring consecutive numbers range in an array
   public static int longestRange(int[] nums) {
     int longestRange = 0;
-    HashMap<Integer, Boolean> num = new HashMap<>(); // Stores a mapping of a number to whether the current number is
-                                                     // part of a particular consecutive range or not
-    for (int x : nums)
-      num.put(x, true);
+    HashMap<Integer, Boolean> num =
+        new HashMap<>(); // Stores a mapping of a number to whether the current number is
+    // part of a particular consecutive range or not
+    for (int x : nums) num.put(x, true);
     for (int x : nums) {
-      if (!num.get(x))
-        continue;
+      if (!num.get(x)) continue;
       num.replace(x, false);
       int currentRange = 1;
       int left = x - 1;
@@ -28,19 +27,19 @@ public class largestRange {
         right++;
       }
       if (currentRange > longestRange) // Store longest range at every interation
-        longestRange = currentRange;
+      longestRange = currentRange;
     }
     return longestRange;
   }
 
   public static void main(String[] args) {
     // Testcases
-    assert longestRange(new int[] { 1, 2, 3, 4, -1, 11, 10 }) == 4;
+    assert longestRange(new int[] {1, 2, 3, 4, -1, 11, 10}) == 4;
     // The longest consecutive number range is of length 4 i.e. {1, 2, 3, 4}
-    assert longestRange(new int[] { -1, 1, 3, 5, 7 }) == 1;
+    assert longestRange(new int[] {-1, 1, 3, 5, 7}) == 1;
     // The longest consecutive number range is of length 1 i.e. any of the element
     // alone
-    assert longestRange(new int[] { 0, 1, 2, 3, 4, 7, 6, 5 }) == 8;
+    assert longestRange(new int[] {0, 1, 2, 3, 4, 7, 6, 5}) == 8;
     // The longest consecutive number range is of length 8 i.e. {0, 1, 2, 3, 4, 5,
     // 6, 7}
   }
